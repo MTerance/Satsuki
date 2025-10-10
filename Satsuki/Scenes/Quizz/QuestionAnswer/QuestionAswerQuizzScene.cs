8e1b3d8c689a8bfc5698531c4adf93141a1de54b
Shomaa Azelkara <sshomaa@gmail.com>
2025-10-11 01:22:29 +0200

    add label logic test for quizz scene

--- Satsuki/Scenes/Quizz/QuestionAnswer/QuestionAswerQuizzScene.cs
+++ Satsuki/Scenes/Quizz/QuestionAnswer/QuestionAswerQuizzScene.cs
@@ -3,9 +3,21 @@ using System;
 
 public partial class QuestionAswerQuizzScene : Control
 {
+	private Label questionLabel;
+	private Label answerLabel;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("QuestionAswerQuizzScene ready");
+		questionLabel = this.GetNode<Label>("QuizzVBoxContainer/QuestionMarginContainer/QuestionLabel");
+		answerLabel = this.GetNode<Label>("QuizzVBoxContainer/AnswerMarginContainer/AnswerLabel");
+		if (questionLabel == null || answerLabel == null)
+		{
+			GD.PrintErr("Labels not found!");
+			return;
+		}
+		questionLabel.Text = "What is the capital of France?";
+		answerLabel.Text = "The capital of France is Paris.";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
