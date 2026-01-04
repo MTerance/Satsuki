59393eb6951d323ab80519952ea5f94220cba41b
Terance <48482758+MTerance@users.noreply.github.com>
2026-01-04 01:23:51 +0100

    changes

--- Ciel/src/App.vue
+++ Ciel/src/App.vue
@@ -8,14 +8,9 @@ import MainGameMenu from './components/gameMenu/MainGameMenu.vue'
 <template>
   <!-- Barre de statut Socket.IO en haut -->
   <MainStatusNavBar />
-  <MainGameMenu />
-  
+  <MainGameMenu />  
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
