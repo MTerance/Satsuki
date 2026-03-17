07a316e97c65fc06daa35cb8963024adb1a22b64
Shomaa Azelkara <sshomaa@gmail.com>
2026-03-17 22:31:20 +0100

    GOAL : Create a Lobby and a system who permits to change the lobby stage

---
+++ Satsuki/Scenes/Lobby.cs
@@ -0,0 +1,22 @@
+using Godot;
+using Satsuki.Models;
+using System;
+
+public partial class Lobby : Node3D
+{
+	private string _lobbyPathScene;
+	private CameraPlacement _cameraPlacement;
+
+    
+	
+	
+	// Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
