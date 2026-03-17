c130b2f559a5957692a0b180f52f04f9a8e26b3d
Shomaa Azelkara <sshomaa@gmail.com>
2026-03-18 00:24:59 +0100

    ajout du gameconfig

--- Satsuki/Models/Spot.cs
+++ Satsuki/Models/Spot.cs
@@ -16,12 +16,23 @@ namespace Satsuki.Models
 	// Spot doit hériter de GodotObject pour être compatible Variant
 	public partial class Spot : GodotObject
 	{
-		public Vector3I Position { get; set; }
+		public Vector3 Position { get; set; }
 		public PlayerTypeSpot PlayerSpot { get; set; }
-		public Spot(Vector3I position, PlayerTypeSpot playerSpot)
+
+		public SpawnPointData SpawnPoint { get; set; }
+
+        public Spot(Vector3 position, PlayerTypeSpot playerSpot, SpawnPointData spawnPoint)
 		{
 			this.Position = position;
 			this.PlayerSpot = playerSpot;
+			this.SpawnPoint = spawnPoint;
 		}
-	}
+
+		public Spot()
+		{
+			this.Position = Vector3.Zero;
+			this.PlayerSpot = PlayerTypeSpot.Vacant;
+			this.SpawnPoint = null;
+        }
+    }
 }
--- Satsuki/Scenes/Lobby.cs
+++ Satsuki/Scenes/Lobby.cs
@@ -1,16 +1,31 @@
 using Godot;
 using Satsuki.Models;
 using System;
+using System.Collections.Generic;
 
 public partial class Lobby : Node3D
 {
 	private string _lobbyPathScene;
 	private CameraPlacement _cameraPlacement;
+	private List<Spot> Spots = new List<Spot>();
 
-    
-	
-	
-	// Called when the node enters the scene tree for the first time.
+	private void LoadLobby(StageResource stageResource)
+	{
+		var lobbyResource = stageResource.LobbyInfo;
+
+		foreach (var spawn in lobbyResource.SpawnPoints)
+		{
+			var spot = new Spot
+			{
+				Position = spawn.Position,
+				PlayerSpot = PlayerTypeSpot.Vacant,
+				SpawnPoint = spawn
+            };
+			Spots.Add(spot);
+        }
+    }
+
+    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
 	}
