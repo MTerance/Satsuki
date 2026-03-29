6bc2595bcc25e03e5e1afd1c41607a51c4431150
Terance <48482758+MTerance@users.noreply.github.com>
2026-03-29 20:58:31 +0200

    MERGE

---
+++ Satsuki/addons/decor_manager/Templates/PlayeZonePlacementContainer.cs
@@ -0,0 +1,16 @@
+using Godot;
+using Satsuki.addons.decor_manager.Models;
+using System;
+
+public partial class PlayeZonePlacementContainer : PanelContainer
+{
+    #region nodes definition
+    private PlayerZoneNode3d PlayerZone { get; set; }
+
+    #endregion
+
+    public override void _Ready()
+    {
+
+    }
+}
