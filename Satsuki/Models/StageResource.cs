5d0fca1e5208ad7e9ba8d15c4e75955e5fc857a9
Terance <48482758+MTerance@users.noreply.github.com>
2026-06-28 14:33:33 +0200

    add access to database for stage builder

--- Satsuki/Models/StageResource.cs
+++ Satsuki/Models/StageResource.cs
@@ -43,8 +43,17 @@ namespace Satsuki.Models
 
 
                         command.CommandText =
-                       $@"REPLACE INTO Stages (ID,NAME,SCENE_NAME,SCENE_PATH,LOBBY_RSC,STAGE_RSC)
-                        VALUES ({this.Id},{this.Name},{this.SceneName},{this.ScenePath},{LobbyRsc},{StageRsc})";
+                       $@"INSERT INTO Stages (ID,NAME,SCENE_NAME,SCENE_PATH,LOBBY_RSC,STAGE_RSC)
+                        VALUES (
+                        CASE WHEN {this.Id} = 0 THEN NULL ELSE {this.Id} END,
+                        '{this.Name}','{this.SceneName}','{this.ScenePath}','{LobbyRsc}','{StageRsc}')
+                        ON CONFLICT(ID)
+                        DO UPDATE SET
+                            NAME = excluded.NAME,
+                            SCENE_NAME = excluded.SCENE_NAME,
+                            SCENE_PATH = excluded.SCENE_PATH,
+                            LOBBY_RSC = excluded.LOBBY_RSC,
+                            STAGE_RSC = excluded.STAGE_RSC;";
                         command.ExecuteNonQuery();
                     }
                 }
@@ -54,7 +63,38 @@ namespace Satsuki.Models
         }
 
 
-
+        public StageResource GetStageById(int id)
+        {
+            using (var dbManager = new SqliteDbManager())
+            {
+                dbManager.OpenConnection();
+                using (var connection = dbManager.GetConnection())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = $"SELECT ID,NAME,SCENE_NAME,SCENE_PATH,LOBBY_RSC,STAGE_RSC FROM Stages WHERE ID = {id};";
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                var stageResource = new StageResource
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    SceneName = reader.GetString(2),
+                                    ScenePath = reader.GetString(3),
+                                    LobbyInfo = JsonSerializer.Deserialize<LobbyInfo>(reader.GetString(4)),
+                                    StageInfo = JsonSerializer.Deserialize<StageInfo>(reader.GetString(5))
+                                };
+                                return stageResource;
+                            }
+                        }
+                    }
+                }
+                dbManager.CloseConnection();
+            }
+            return null;
+        }
 
         public bool Save(string path)
         {
---
+++ Satsuki/Repositories/Builders/StageResourceBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satsuki.Repositories.Builders
+{
+    public class StageResourceBuilder
+    {
+        public List<Tuple<int, string>> GetStages()
+        {
+            var stages = new List<Tuple<int, string>>();
+            using (var dbManager = new SqliteDbManager())
+            {
+                dbManager.OpenConnection();
+                using (var connection = dbManager.GetConnection())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT ID, NAME FROM Stages;";
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int id = reader.GetInt32(0);
+                                string name = reader.GetString(1);
+                                stages.Add(new Tuple<int, string>(id, name));
+                            }
+                        }
+                    }
+                }
+                dbManager.CloseConnection();
+            }
+            return stages;
+        }
+    }
+}
