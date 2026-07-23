a7d7a6a3cf6870a1c925553433d3a9b543fcb23d
Terance Moussoki <terance.moussoki@gmail.com>
2026-07-23 23:09:15 +0200

    creation of folder model for QuestionAnswer

--- Satsuki/Scenes/Quizz/QuestionAnswer/QuestionAswerQuizzScene.cs
+++ Satsuki/Scenes/Quizz/QuestionAnswer/QuestionAswerQuizzScene.cs
@@ -2,33 +2,9 @@ using Godot;
 using Godot.Collections;
 using Satsuki.Interfaces.Quizz;
 using Satsuki.Models.Resources;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http.Json;
+using Satsuki.Scenes.Quizz.QuestionAnswer.Models;
 using System.Text.Json;
 
-public class QuestionAnswerQuizzModel : IQuizzModel
-{
-	public enum ShowStateMedia
-	{
-		BeforeQuestion,
-		BeforeAnswer,
-		DuringQuestion,
-		DuringAnswer,
-		AfterAnswer
-	}
-
-	public string Question { get; set; }
-	public string Answer { get; set; }
-	public System.Collections.Generic.Dictionary<ShowStateMedia, MediaModel> Medias { get; set; } // Path to media file (image, audio, video)
-	public QuestionAnswerQuizzModel(string question, string answer, System.Collections.Generic.Dictionary<ShowStateMedia, MediaModel> medias = null)
-	{
-		Question = question;
-		Answer = answer;
-		Medias = medias ?? new System.Collections.Generic.Dictionary<ShowStateMedia, MediaModel>();
-	}
-}
 
 public partial class QuestionAswerQuizzScene : Control, IQuizz
 {
